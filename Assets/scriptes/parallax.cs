a225f8dd2f1046427808a3fede80a1d5816712cc
salouta1987 <112333794+salouta1987@users.noreply.github.com>
2022-12-03 17:03:46 +0100

    Initial commit

---
+++ Assets/scriptes/parallax.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class parallax : MonoBehaviour
+{
+
+    private MeshRenderer meshRenderer;
+
+    public float animationSpeed = 1f;
+
+
+    private void Awake()
+    {
+        meshRenderer = GetComponent<MeshRenderer>();
+    }
+
+    private void Update()
+    {
+        meshRenderer.material.mainTextureOffset += new Vector2(animationSpeed * Time.deltaTime, 0);
+    }
+
+
+
+
+
+
+
+
+}
